f67b6a2fdce91b5fb5a9ffe3a3da6f7b5a6724f5
ziye2017 <xsjxsr@163.com>
2017-01-19 11:26:28 +0800

    get headers info message

--- index.js
+++ index.js
@@ -22,6 +22,18 @@ var fortunes = [
 ];
 
 
+app.get('/headers', function(req, res){
+	
+	res.set('Content-Type', 'text/plain');
+	var s = '';
+	for (var name in req.headers)
+	{
+		s += name + ": " + req.headers[name] + '\n';
+	}
+	res.send(s);
+
+});
+
 
 app.get('/about', function(req, res, next){
 	
